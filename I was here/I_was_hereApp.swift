0b2687ed6d527b4b578cbe288292d6e079624589
Marco Carandente <marco.carandente@gmail.com>
2023-07-13 21:46:58 +0300

    Initial Commit

---
+++ I was here/I_was_hereApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_was_hereApp.swift
+//  I was here
+//
+//  Created by Marco Carandente on 13.7.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_was_hereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
