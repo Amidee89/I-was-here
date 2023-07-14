1855d564132d60f4588433446e7f2b66555001de
Marco Carandente <marco.carandente@gmail.com>
2023-07-14 19:00:12 +0300

    Initial GPX processing

--- I was here/ContentView.swift
+++ I was here/ContentView.swift
@@ -12,6 +12,7 @@ import AppKit
 import UIKit
 #endif
 import UniformTypeIdentifiers
+import CoreGPX
 
 extension UTType {
     static var gpx: UTType {
@@ -29,22 +30,65 @@ struct FilePickerView: View {
     @State private var isPickerPresented = false
     var body: some View {
         VStack {
-            Button("Select GPX Folder") {
+            Button("Select GPX Folder or files") {
                 isPickerPresented = true
             }
         }
         .fileImporter(
             isPresented: $isPickerPresented,
             allowedContentTypes: [.folder, .gpx],
-            allowsMultipleSelection: false
+            allowsMultipleSelection: true
         ) { result in
             do {
-                let folderURL = try result.get().first!
-                // Do something with the selected folder URL
-            } catch {
-                // Handle failure cases here
-            }
+                   let urls = try result.get()
+                   processFiles(at: urls)
+               }
+            catch {
+                   // Handle failure cases here
+               }
         }
+    }
+}
+
+func processFiles(at urls: [URL]) {
+    for url in urls {
+        if url.pathExtension == "gpx" {
+            processGPXFile(at: url)
+        } else if url.hasDirectoryPath {
+            processFilesInFolder(at: url)
+        }
+    }
+}
+
+func processGPXFile(at url: URL) {
+    do {
+        let fileAttributes = try FileManager.default.attributesOfItem(atPath: url.path)
+        if let fileSize = fileAttributes[FileAttributeKey.size] as? NSNumber {
+            print("File size: \(fileSize.intValue) bytes")
+        }
+    } catch {
+        print("Failed to get file size for \(url.path)")
+    }
+
+    if let gpx = GPXParser(withURL: url)?.parsedData() {
+        let trackCount = gpx.tracks.count
+        print("Number of tracks: \(trackCount)")
+
+        for (index, track) in gpx.tracks.enumerated() {
+            let trackpointsCount = track.tracksegments.flatMap { $0.trackpoints }.count
+            print("Track \(index + 1) has \(trackpointsCount) trackpoints")
+        }
+    }
+}
+
+func processFilesInFolder(at url: URL) {
+    let fileManager = FileManager.default
+    do {
+        let contents = try fileManager.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: [])
+        let gpxFiles = contents.filter { $0.pathExtension == "gpx" }
+        processFiles(at: gpxFiles)
+    } catch {
+        // Handle failure to read the directory here
     }
 }
 
